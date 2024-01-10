4b0f3d1aa4e80af326cf5977120e6531d5022924
ifixu <142580104+ifixu@users.noreply.github.com>
2024-01-10 19:37:35 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -27,7 +27,7 @@ class TestHootel(object):
     @allure.tag("login")
     def test_login(self, email, password):
         menu_toggle = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="navbar-toggler collapsed"]')))
-        menu_toggle.click
+        menu_toggle.click()
         login_btn = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable((By.XPATH, '//a[@class="nav-link"]')))
         login_btn.click()
 
